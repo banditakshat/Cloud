e9fbe649d448016addae2b142a9fadb55d485a7e
kyle161728 <98822621+kyle161728@users.noreply.github.com>
2022-04-24 21:41:38 +0530

    Add files via upload

---
+++ cloud.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+while getopts t:w: flag
+
+do
+        case "${flag}" in
+                t) target=${OPTARG}
+                        ;;
+                w) wordlist=${OPTARG}
+                         ;;
+                *) echo "Invalid option: -$flag" ;;
+        esac
+done
+
+if [[ $target == "" ]]; then
+    echo "You must provide a -t flag";
+    exit 1;
+elif [[ $wordlist == "" ]]; then
+    echo "you must provide a -w flag";
+    exit 1;
+fi
+
+echo "S3recon Starting...................................................";
+mkdir -p $target/s3;
+s3recon $wordlist --public | grep PUBLIC | cut -d " " -f 3 | tee $target/s3recon/s3recon.txt
+
+echo "Slurp Starting.....................................................";
+mkdir  -p $target/s3 ;
+wget https://github.com/0xbharath/slurp/blob/master/permutations.json -O $target/s3/permutations.json;
+slurp domain -c 25 -p $target/s3/permutations.json -t $target >>  $target/s3/slurp.txt;
+
+echo "clubbing all.................................................";
+cat $target/s3/*.txt |sort -u | grep $target | tee $target/s3/RawS3.txt;
+
+echo "S3scanner Starting.................................................";
+mkdir -p $target/s3/dump ;
+s3scanner scan --threads 8 --dangerous --buckets-file $target/s3/RawS3.txt | grep bucket_exist | tee $target/s3/s3scanner.txt ;
+s3scanner dump -f $target/s3/s3scanner.txt  -d $target/s3/dump;
+
+
+
+
+
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pip3 install s3scanner;
+pip3 install s3recon;
+wget https://github.com/0xbharath/slurp/releases/download/1.1.0/slurp-1.1.0-linux-amd64 ;
+mv slurp-1.1.0-linux-amd64 slurp;
+chmod +x slurp ;
+sudo cp slurp /usr/bin/ ;
+echo "before using script, first configure aws cli for better results"
