44501566e2244ade76b995ade58671cbcb2e11e5
banditakshat <98822621+banditakshat@users.noreply.github.com>
2022-05-30 16:51:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ wget https://github.com/0xbharath/slurp/releases/download/1.1.0/slurp-1.1.0-linu
 mv slurp-1.1.0-linux-amd64 slurp;
 chmod +x slurp ;
 sudo cp slurp /usr/bin/ ;
-echo "before using script, first configure aws cli for better results"
+echo "before using the cloud.sh script, first configure aws cli for better results"
